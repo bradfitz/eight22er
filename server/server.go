538e5613707e9bed0db6e17357fc0023424fd7b0
Brad Fitzpatrick <brad@danga.com>
2011-12-11 12:05:10 -0800

    Working OAuth requests to twitter.

--- server/server.go
+++ server/server.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"sort"
 	"strings"
 
 	"eight22er/oauth"
@@ -14,12 +20,62 @@ func main() {
 
 	oc := &oauth.Client{
 		Credentials: oauth.Credentials{
-			Token:  slurpFile("config-token"),
-			Secret: slurpFile("config-tokensecret"),
+			Token:  slurpFile("config-consumerkey"),
+			Secret: slurpFile("config-consumersecret"),
 		},
 	}
+	cred := &oauth.Credentials{
+		Token:  slurpFile("config-token"),
+		Secret: slurpFile("config-tokensecret"),
+	}
 
-	log.Printf("config: %#v", oc)
+	urlBase := "https://api.twitter.com/1/direct_messages.json"
+	params := make(url.Values)
+	params.Set("count", "50")
+	oc.SignParam(cred, "GET", urlBase, map[string][]string(params))
+
+	authHeader := buildAuthHeader(params)
+	reqURL := fmt.Sprintf("%s?%s", urlBase, params.Encode())
+
+	log.Printf("Req URL: %s", reqURL)
+	log.Printf("Authorization: %s", authHeader)
+
+	req, _ := http.NewRequest("GET", reqURL, nil)
+	req.Header.Add("Authorization", authHeader)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Get: %v", err)
+	}
+	io.Copy(os.Stderr, res.Body)
+}
+
+func buildAuthHeader(vals url.Values) string {
+	var buf bytes.Buffer
+	if _, ok := vals["oauth_version"]; !ok {
+		vals.Set("oauth_version", "1.0")
+	}
+	fmt.Fprintf(&buf, "OAuth")
+	remove := []string{}
+	for k := range vals {
+		if !strings.HasPrefix(k, "oauth_") {
+			continue
+		}
+		remove = append(remove, k)
+	}
+	sort.Strings(remove)
+	for n, k := range remove {
+		if n > 0 {
+			buf.WriteByte(',')
+		}
+		v := vals.Get(k)
+		if k == "oauth_signature" {
+			v = url.QueryEscape(v)
+		}
+		fmt.Fprintf(&buf, " %s=%q", k, v)
+		delete(vals, k)
+	}
+	return buf.String()
 }
 
 func slurpFile(file string) string {
