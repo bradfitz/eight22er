81e3217f6dea9e478c47f8144c04e64b4e072542
Brad Fitzpatrick <brad@danga.com>
2011-12-11 14:15:40 -0800

    making a Conn type

--- server/server.go
+++ server/server.go
@@ -1,23 +1,136 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"net/textproto"
 	"net/url"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 
 	"eight22er/oauth"
 )
 
+var (
+	popPort  = flag.Int("pop_port", 1100, "POP3 plaintext port")
+	smtpPort = flag.Int("smtp_port", 2500, "SMTP plaintext port")
+)
+
 func main() {
 	log.Printf("server.")
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(*popPort))
+	check(err)
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Printf("Accept error, shutting down: %v", err)
+			return
+		}
+		go NewConn(c).serve()
+	}
+}
+
+type pop3State int
+
+const (
+	authState pop3State = iota
+	txState
+)
+
+func splitPOP3Line(s string) (cmd, params string) {
+	v := strings.SplitN(s, " ", 2)
+	if len(v) < 2 {
+		return v[0], ""
+	}
+	return strings.TrimSpace(v[0]), strings.TrimSpace(v[1])
+}
+
+func NewConn(c net.Conn) *Conn {
+	br := bufio.NewReader(c)
+	bw := bufio.NewWriter(c)
+	return &Conn{
+		Conn:  c,
+		br: br,
+		bw: bw,
+		tr: textproto.NewReader(br),
+	}
+}
+
+type Conn struct {
+	net.Conn
+	br *bufio.Reader
+	bw *bufio.Writer
+	tr *textproto.Reader
+}
+
+func (c *Conn) send(s string) {
+	c.bw.WriteString(s)
+	if !strings.HasSuffix(s, "\r\n") {
+		c.bw.WriteString("\r\n")
+	}
+	c.bw.Flush()
+}
+
+func (c *Conn) err(s string) {
+	c.send(fmt.Sprintf("-ERR %s", s))
+}
+
+func (c *Conn) disconnect(s string) error {
+	c.send(fmt.Sprintf("-ERR %s", s))
+	return errors.New("Client error: " + s)
+}
+
+func (c *Conn) serve() error {
+	log.Printf("New connection from %q", c.RemoteAddr())
+	defer c.Close()
+
+	c.send("+OK POP3 eight22er here, ready to proxy your DMs, yo")
+
+	state := authState
+	var user, password string
+	for {
+		line, err := c.tr.ReadLine()
+		if err != nil {
+			log.Printf("Error reading from connection: %v", err)
+			return err
+		}
+		cmd, params := splitPOP3Line(line)
+		log.Printf("Got line: %q, cmd %q, params %q", line, cmd, params)
+		switch cmd {
+		case "AUTH", "CAPA":
+			if state != authState {
+				return c.disconnect(fmt.Sprintf("Bogus %s command in wrong state", cmd))
+			}
+			log.Printf("Auth line: %q", line)
+			c.err(fmt.Sprintf("let's pretend I don't know the %s extension", cmd))
+		case "USER":
+			if state != authState {
+				return c.disconnect(fmt.Sprintf("Bogus %s command in wrong state", cmd))
+			}
+			user = params
+			c.send("+OK")
+		case "PASS":
+			if state != authState {
+				return c.disconnect(fmt.Sprintf("Bogus %s command in wrong state", cmd))
+			}
+			password = params
+			c.send("+OK")
+		default:
+			log.Printf("UNHANDLED COMMAND %q, params %q", cmd, params)
+		}
+	}
+	log.Printf("user = %q, password = %q", user, password)
 
 	oc := &oauth.Client{
 		Credentials: oauth.Credentials{
@@ -51,6 +164,8 @@ func main() {
 	for _, dm := range dms {
 		fmt.Printf("From: %s (%s)\t%q\n", dm.Sender().ScreenName(), dm.Sender().Name(), dm.Text())
 	}
+
+	return nil
 }
 
 type DM map[string]interface{}
