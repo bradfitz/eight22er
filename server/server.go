ed84e9032372422848c4aa77e174c87b94ff9b3c
Brad Fitzpatrick <brad@danga.com>
2011-12-11 11:23:01 -0800

    start of building oauth config

--- server/server.go
+++ server/server.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
-	_ "eight22er/oauth"
+	"eight22er/oauth"
 )
 
 func main() {
 	log.Printf("server.")
+
+	oc := &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  slurpFile("config-token"),
+			Secret: slurpFile("config-tokensecret"),
+		},
+	}
+
+	log.Printf("config: %#v", oc)
+}
+
+func slurpFile(file string) string {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalf("Error opening %q: %v", file, err)
+	}
+	defer f.Close()
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(string(bs))
 }
